5a28c6c5a6071e6dfa857091924d1dae8742973d
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-07-19 16:16:31 +0530

    dark mode

--- app/assets/Context/page.tsx
+++ app/assets/Context/page.tsx
@@ -1,22 +1,15 @@
 'use client'
-
-import { createContext, useContext, useState, ReactNode } from "react";
-
-interface ContextProps {
-  dark: boolean;
-  setDark: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-const CrtContext = createContext<ContextProps | undefined>(undefined);
-
-export const AppWrapper = ({children}: {children: ReactNode}) => {
-    const [dark, setDark] = useState(false);
-
+import { createContext, useContext, useState } from "react";
+const CrtContext = createContext<any>(undefined);
+const AppWrapper = ({children}: Readonly<{children: React.ReactNode}>) => {
+    let [dark, setDark] = useState(false);
     return (
-        <CrtContext.Provider value={{dark, setDark}}>
-            {children}
-        </CrtContext.Provider>
-    );
+        <>
+            <CrtContext.Provider value={{dark,setDark}}>
+                {children}
+            </CrtContext.Provider>
+        </>
+    )
 }
-
-export const UseCont = () => useContext(CrtContext)
+export default AppWrapper;
+export const UseCont=()=>useContext(CrtContext)
