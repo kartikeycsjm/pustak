790babf6ede57ac71ea1cbe35395004b093c39ed
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-07-19 16:19:45 +0530

    changes layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from './assets/Header'
-import { AppWrapper } from "./assets/Context/page";
+import AppWrapper from "./assets/Context/page";
 import Footer from "./assets/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
